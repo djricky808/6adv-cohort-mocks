f21faa1ce5ea8df5d1938aa689f83388597929d2
Richard Olpindo <djricky088@yahoo.com>
2025-03-14 17:59:07 -1000

    Completed first hour of practice

--- mock-1/mock-1.ts
+++ mock-1/mock-1.ts
@@ -9,6 +9,42 @@
 // # Input: numbers = [1, 3, 4, 5, 7], target = 8
 // # Output: [2, 4]
 
+function twoSum(numbers: number[], target: number): number[] | null | void {
+  let left: number = 0;
+  let right: number = numbers.length - 1;
+  function findTwoSum(
+    numbers: number[],
+    left: number,
+    right: number,
+    target: number
+  ): number[] | null | void {
+    console.log(
+      "indices L/R:",
+      left,
+      right,
+      "numbers:",
+      numbers[left],
+      numbers[right]
+    );
+    if (left > right || numbers.length < 2) {
+      return null;
+    }
+    if (numbers[left] + numbers[right] === target) {
+      console.log("The numbers match the target!");
+      return [left + 1, right + 1];
+    }
+    if (numbers[left] + numbers[right] > target) {
+      return findTwoSum(numbers, left, right - 1, target);
+    }
+    if (numbers[left] + numbers[right] < target) {
+      return findTwoSum(numbers, left + 1, right, target);
+    }
+  }
+  return findTwoSum(numbers, left, right, target);
+}
+
+// console.log(twoSum([2], 9));
+
 // # Longest Substring Without Repeating Characters
 // # Given a string, find the length of the longest substring without repeating characters using the sliding window technique.
 // # Example:
@@ -17,3 +53,37 @@
 // # Input: "bbbbb"
 // # Output: 1  ("b")
 
+function longestSubstring(string: string): number {
+  if (string.length <= 1) {
+    return string.length;
+  }
+  let characters: Set<string> = new Set();
+  let left = 0;
+  let right = 0;
+  let maxLength = 0;
+  function findLongestString(
+    string: string,
+    left: number,
+    right: number,
+    maxLength: number
+  ): number {
+    console.log("Left,right:", left, right);
+    if (right === string.length - 1) {
+      return maxLength;
+    }
+    while (characters.has(string[right])) {
+      characters.delete(string[right]);
+      left++;
+    }
+    characters.add(string[right]);
+    right++;
+    return findLongestString(
+      string,
+      left,
+      right,
+      Math.max(right - left, maxLength)
+    );
+  }
+  return findLongestString(string, left, right, maxLength);
+}
+console.log(longestSubstring(""));
